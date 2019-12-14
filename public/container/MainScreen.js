5d369611d216215135cc12a841e8d04d84e07ae1
youtaebong <ytb4748@gmail.com>
2019-12-14 19:32:12 +0900

    default set

---
+++ public/container/MainScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class MainScreen extends React.Component {
+
+
+    render() {
+        return (
+            <View>
+                <Text>MainScreen</Text>
+            </View>
+        )
+
+    }
+}
+
+
+export default MainScreen;
